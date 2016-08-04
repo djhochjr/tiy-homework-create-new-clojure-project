c3c3d563ae0f35c6030700f8274b48fc163a9acf
dave hochstrasser <davehochstrasser@daves-MacBook-Pro.local>
2016-08-04 08:29:08 -0400

    first commit

--- src/tiy_homework_create_new_clojure_project/core.clj
+++ src/tiy_homework_create_new_clojure_project/core.clj
@@ -1,7 +1,28 @@
 (ns tiy-homework-create-new-clojure-project.core)
 
 (defn -main[]
-  (println "Hello World")
+    (let [firstName "Dave"
+          lastName "Hochstrasser"
+          streetAddress "1669 Alta Way"
+          city "Salt Lake City"
+          state "Utah"
+          zip "88580"
+          ]
+      ;;assigns items names
+    (println firstName)                                   ;
+    (println lastName)                                    ;
+    (println streetAddress)                             ;
+    (println city)                                      ;
+    (println state)                                     ;
+    (println zip)                                       ;
+      ;;prints the names of the items
+    (println "Thiis is where i'd like to live")             ;
+      ;;prints words
+
+      ))
+(-main)
+
+
 
 
 
